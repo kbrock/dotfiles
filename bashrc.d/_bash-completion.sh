a41e4a907b30b7dd574486070bbc0ffcc79c808b
Keenan Brock <keenan@thebrocks.net>
2020-07-29 14:19:38 -0400

    fix bashcompletion to include git

--- bashrc.d/_bash-completion.sh
+++ bashrc.d/_bash-completion.sh
@@ -1,1 +1,2 @@
-source /usr/local/share/bash-completion/bash_completion
+[[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+# source /usr/local/share/bash-completion/bash_completion
