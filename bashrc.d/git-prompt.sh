b95fa56440d10a58210e7018a9dffdad54005a8b
Keenan Brock <keenan@thebrocks.net>
2023-04-25 10:44:00 -0400

    git prompt tweaks
    - comment about other existing git prompt script
    - use [[]] over [] (bash internal vs external program)
    - merged local dir with git_dir
    - no longer exporting git_dir
    - PROMPT_COMMAND has space before semicolon
    - previously empty PROMPT_COMMAND no longer adds extra semicolon
    - comments for colors

--- bashrc.d/git-prompt.sh
+++ bashrc.d/git-prompt.sh
@@ -1,21 +1,30 @@
-#export PS1='$(git-PS1 "\W \$ ")'
-
-# thinking maybe I should use the other
+# possibly go with the standard git prompt:
+if false ; then
+  GIT_PS1_SHOWCOLORHINTS=true
+  [[ "$PROMPT_COMMAND" != *"__git_ps1"* ]] && export PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ ";'" $PROMPT_COMMAND"
+fi
 
 # thanks from: http://aaroncrane.co.uk/2009/03/git_branch_prompt/
+# may want to go with rev-parse instead of traversal.
+# these can all be run in a single call
+#
+# git_dir=$(git rev-parse --git-dir)
+# head=$(git rev-parse --symbolic-full-name HEAD)
+# git_loc=$(git rev-parse --show_prefix)
+# maybe: git --diff-files => git status -sb
+
 function find_git_branch {
-  local dir=. head
-  git_dir=${PWD}
-  #TODO: replace dir with git_dir and get rid of $dir
-  until [ "$dir" -ef / ]; do
-    if [ -f "$dir/.git/HEAD" ]; then
-      head=$(< "$dir/.git/HEAD")
-    elif [ -f "$dir/.git" ] ; then
-      gitref=$(< "$dir/.git")
+  local head git_dir=${PWD}
+
+  until [[ -z "$git_dir" || "$git_dir" = / ]] ; do
+    if [[ -f "$git_dir/.git/HEAD" ]] ; then
+      head=$(< "$git_dir/.git/HEAD")
+    elif [[ -f "$git_dir/.git" ]] ; then
+      local gitref=$(< "$git_dir/.git")
       head=$(< "${gitref#* }/HEAD")
     fi
 
-    if [ -n "$head" ] ; then
+    if [[ -n "$head" ]] ; then
       if [[ $head == ref:\ refs/heads/* ]]; then
         git_branch="${head#*/*/}"
       elif [[ $head != '' ]]; then
@@ -27,30 +36,30 @@ function find_git_branch {
       git_loc="${PWD/${git_dir}}"
 
       # wish there was a way to do this not running all these commands
+      # also, they don't quite update until `git status` is run
       # note: these fail when running in .git directory
       if ! $(git diff-files --ignore-submodules --quiet 2> /dev/null) ; then
-        #locally modified
-        git_status=31
+        git_status=31 # red - locally modified
       elif ! $(git diff-index --ignore-submodules --cached --quiet HEAD -- 2> /dev/null) ; then
-        #committed, but not pushed
-        git_status=32
+        git_status=32 # green - staged
       else
-        git_status=36
+        git_status=36 # cyan - pristine
       fi
+      # TODO: git_status="\033[${git_status}m"
+      # TODO: ? export PS1=
       return
-    else
+    else # keep looking
       git_dir="${git_dir%/*}"
-      dir="../$dir"
     fi
   done
-# exported variables
+  # not in a git directory (set variables accordingly)
+  # TODO: git_status=\033[36m
   git_status=36
-  git_dir=''
   git_branch=''
   git_root=''
   git_loc=${PWD##*/}
+  # TODO: ? export PS1=
 }
 
-[[ "$PROMPT_COMMAND" != *"find_git_branch"* ]] && export PROMPT_COMMAND="find_git_branch; $PROMPT_COMMAND"
-#TODO: return status - emoji?
+[[ "$PROMPT_COMMAND" != *"find_git_branch"* ]] && export PROMPT_COMMAND="find_git_branch${PROMPT_COMMAND:+ ; }${PROMPT_COMMAND}"
 export PS1="\${git_branch:+[}\[\033[\${git_status}m\]\${git_branch}\[\033[0m\]\${git_branch:+] }\[\033[1;34m\]\${git_root}\[\033[0m\]\${git_loc} $ "
