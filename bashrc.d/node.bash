ae5746ff21c9320ffec82baff4211144c494736d
Keenan Brock <keenan@thebrocks.net>
2023-04-25 17:10:36 -0400

    add node support /via nvm

--- bashrc.d/node.bash
+++ bashrc.d/node.bash
@@ -1,4 +1,9 @@
-# /usr/local/lib/node_modules
-# /usr/local/share/npm/bin
+
+mkdir -p ~/.nvm
+export NVM_DIR=~/.nvm
+
+# $(brew --prefix nvm)/nvm.sh
+source ${HOMEBREW_PREFIX}/opt/nvm/nvm.sh
+
 [ -d ${HOMEBREW_PREFIX}/lib/node_modules ] && NODE_PATH=$NODE_PATH:${HOMEBREW_PREFIX}/lib/node_modules
 add_to_path ${HOMEBREW_PREFIX}/share/npm/bin
