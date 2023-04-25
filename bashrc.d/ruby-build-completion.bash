dee7a853ac4e5ee510f025ce4f5d207b0a097134
Keenan Brock <keenan@thebrocks.net>
2023-04-25 13:23:04 -0400

    fixup ruby-build-completion.bash with extra tabs
    Did throw in an `x` for the dash completions.
    That is only there so it doesn't automatically add a dash.
    The x is not valid.

--- bashrc.d/ruby-build-completion.bash
+++ bashrc.d/ruby-build-completion.bash
@@ -7,28 +7,23 @@ _ruby_build_complete() {
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
   # previous is empty
-  if [[ "${prev}" = "ruby-build" ]] ; then
-    case ${cur} in
-      -*) COMPREPLY=($(compgen -W '-k --keep -v --verbose -p --patch -4 \
-                                   --ipv4 -6 --ipv6 --definitions' -- $cur )) ; return 0 ;;
-    esac
-
-
-    # if they haven't specified anything yet, assume 2.x
-    COMPREPLY=($(compgen -W "`ruby-build --definitions`" -- ${cur:-2}))
+  if [[ "${cur}" = -* || "${prev}" = /* ]] ; then
+    # the x is bogus, but it prevents a default - from being added
+    COMPREPLY=($(compgen -W 'x -k --keep -v --verbose -p --patch -4 \
+                                --ipv4 -6 --ipv6 --definitions' -- $cur ))
     return 0
-  else
-    # echo "cur=${cur}"
-    # echo "prev=${prev}"
-    # return 0
-
+  elif [[ "${prev}" = "ruby-build" ]] ; then
+    # suggest a version of ruby to use
+    # if they haven't specified anything yet, assume 3.x
+    COMPREPLY=($(compgen -W "`ruby-build --definitions`" -- ${cur:-3}))
+    return 0
+  else # if [[ "" = "" ]] ; then
     case ${prev} in
       [a-z]*-dev) target="${prev%-dev}-$(date +"%Y%m%d")" ;;
       [a-z]*) target="${prev}" ;;
       *) target="ruby-${prev}" ;;
     esac
 
-    # was /opt/rubies/${target}
     COMPREPLY=( $(compgen -W "~/.rubies/${target}" -- ${cur}) )
     return 0
   fi
