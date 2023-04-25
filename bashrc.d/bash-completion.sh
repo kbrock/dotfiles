c428c0a5171be5487949d476f3586da85c7a60d8
Keenan Brock <keenan@thebrocks.net>
2023-04-25 10:25:06 -0400

    remove duplicate bash completion

--- bashrc.d/bash-completion.sh
+++ bashrc.d/bash-completion.sh
@@ -1,2 +1,2 @@
+export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
 [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
-# source /usr/local/share/bash-completion/bash_completion
