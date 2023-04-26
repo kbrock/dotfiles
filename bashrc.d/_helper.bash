bc900964d2c36c675557fb18456929e1ed4e0673
Keenan Brock <keenan@thebrocks.net>
2023-04-25 23:56:37 -0400

    manually set path for homebrew
    only saving 20ms on this one. So maybe it is unnecessary optimization.
    So 80ms => 60ms

--- bashrc.d/_helper.bash
+++ bashrc.d/_helper.bash
@@ -22,14 +22,30 @@ function add_to_path() {
   not_in_path "$1" && export PATH="$PATH:$1"
 }
 
-# adds homebrew to PATH and HOMEBREW_PREFIX
-[[ -f /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+function add_to_early_path() {
+  not_in_path "$1" && export PATH="$1:$PATH"
+}
+
+# adds homebrew to PATH, MANPATH, INFOPATH, and HOMEBREW_PREFIX
+# alt:
+#[[ -f /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+
 add_to_path /usr/local/bin
+if [[ -f /opt/homebrew/bin/brew ]] ; then
+  export HOMEBREW_PREFIX="/opt/homebrew";
+  add_to_early_path "${HOMEBREW_PREFIX}/bin"
+  add_to_early_path "${HOMEBREW_PREFIX}/sbin"
+  # trailing : is important
+  export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:"
+  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
+elif [[ -f /usr/local/bin/brew ]] ; then
+  export HOMEBREW_PREFIX="/usr/local"
+fi
 
 # if we have homebrew installed, but not running homebrew bash
 if [[ "$SHELL" = "/bin/bash" && -n "$HOMEBREW_PREFIX" ]] ; then
   echo
-  echo -e "\033[31mwarning\033[0m using non-homebrew ${SHELL}. \033[31mrun\033[0m:"
+  echo -e "\033[31mwarning\033[0m using non-homebrew ${SHELL}. expecting $(which bash). \033[31mrun\033[0m:"
   grep -q $(which bash) /etc/shells || echo "sudo echo \$(which bash) >> /etc/shells"
   echo "chsh -s \$(which bash)"
   echo
