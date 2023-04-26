0a02b2db5d71d79d867e5274197a143ad55092ff
Keenan Brock <keenan@thebrocks.net>
2023-04-25 21:30:42 -0400

    document how to setup/configure nvm

--- bashrc.d/node.bash
+++ bashrc.d/node.bash
@@ -1,6 +1,12 @@
 
-mkdir -p ~/.nvm
 export NVM_DIR=~/.nvm
+
+# TO configure nvm:
+# mkdir ${NVM_DIR}
+# source $(brew --prefix nvm)/nvm.sh
+# nvm install 18.0.0
+# nvm default 18.0.0
+# ln -s ${HOMEBREW_PREFIX}/etc/bash_completion.d/nvm ${NVM_DIR}/bash_completion
 
 # $(brew --prefix nvm)/nvm.sh
 source ${HOMEBREW_PREFIX}/opt/nvm/nvm.sh
