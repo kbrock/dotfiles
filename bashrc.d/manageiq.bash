7a3744616dda62a906c6498ac8d6bc45cd649d11
Keenan Brock <keenan@thebrocks.net>
2024-11-13 11:06:50 -0500

    repos, plugins - basic providers cmd clone

--- bashrc.d/manageiq.bash
+++ bashrc.d/manageiq.bash
@@ -3,9 +3,14 @@ api_token() {
   export AUTH_HEADER="X-Auth-Token: ${AUTH_TOKEN}"
 }
 
+# These are all the ems providers
+# To create:
+#   sed 's/"//g' ~/src/manageiq/Gemfile | awk '/plugin.*-providers-/ {printf "%s\n", $2}' | sort > ~/src/providers2
+#
 # providers -x "[ \$(git branch-name) = 'master' ] && echo \$i"
 # providers -t "[ \$(git branch-name) = 'master' ]
 # providers git status
+#
 providers() {
   local v1 i
   case $1 in
@@ -22,4 +27,36 @@ providers() {
   done
 }
 
-# sed 's/"//g' ~/src/manageiq/Gemfile | awk '/plugin.*-providers-/ {printf "%s\n", $2}' > ~/src/providers2
+# think using plugins may be better?
+repos() {
+  local v1 i f
+  case $1 in
+    -t) shift ; v1=true ;;
+    -f) shift ; v1=false ;;
+    -x|-q) shift ; v1=quiet ;;
+  esac
+  for f in ~/src/{manageiq,manageiq-*,inventory_refresh} ; do
+    i=${f##*/}
+    [[ -z "${v1}" ]] && echo " === $i ==="
+    pushd ~/src/$i > /dev/null
+    eval "$*"
+    [[ "${v1}" == "true" && ${?} = 0 || "${v1}" == "false" && ${?} != 0 ]] && echo " === $i ==="
+    popd > /dev/null
+  done
+}
+
+plugins() {
+  local v1 i
+  case $1 in
+    -t) shift ; v1=true ;;
+    -f) shift ; v1=false ;;
+    -x|-q) shift ; v1=quiet ;;
+  esac
+  for i in $(grep -v '#' ~/src/plugins) ; do
+    [[ -z "${v1}" ]] && echo " === $i ==="
+    pushd ~/src/$i > /dev/null
+    eval $*
+    [[ "${v1}" == "true" && ${?} = 0 || "${v1}" == "false" && ${?} != 0 ]] && echo " === $i ==="
+    popd > /dev/null
+  done
+}
