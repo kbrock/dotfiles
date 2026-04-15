8ad83889efbe4ca609d8e332c229efe0972127af
Keenan Brock <keenan@thebrocks.net>
2026-04-15 15:35:43 -0400

    add discover back. default to 4.0

--- bashrc.d/chruby.sh
+++ bashrc.d/chruby.sh
@@ -2,6 +2,25 @@
 
 if [[ -d ${HOMEBREW_PREFIX}/share/chruby ]] ; then
   . ${HOMEBREW_PREFIX}/share/chruby/chruby.sh
-  # chruby.xtra
   . ${HOMEBREW_PREFIX}/share/chruby/auto.sh
+  chruby 4.0
+
+  chruby_discover() {
+    local dir
+    RUBIES=()
+    for dir in "$PREFIX/opt/rubies" "$HOME/.rubies"; do
+      [[ -d "$dir" && -n "$(ls -A "$dir")" ]] && RUBIES+=("$dir"/*)
+    done
+  }
+
+  _chrubycomplete() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local rubies="${RUBIES[@]##*/}"
+    if [[ $COMP_CWORD -eq 1 ]]; then
+      COMPREPLY=($( compgen -W "$rubies" -- $cur ))
+    fi
+  }
+  complete -o nospace -F _chrubycomplete chruby
+else
+  echo "warning: chruby not found (HOMEBREW_PREFIX=${HOMEBREW_PREFIX:-unset})" >&2
 fi
